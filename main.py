19aca7bb0c1ae0f94e493833e4a3721eb77ebc89
Diego Desvignes <diegodesvignes10@gmail.com>
2026-08-08 16:24:00 +0200

    Ajout de l'option recherche complète au menu (version 1.2)

--- main.py
+++ main.py
@@ -55,21 +55,24 @@ def menu():
 
     1 - Ajouter un contact
     2 - Supprimer un contact
-    3 - Afficher tous les contacts
-    4 - Quitter
+    3 - Rechercher un contact
+    4 - Afficher tous les contacts
+    5 - Quitter
 """)
 
 
 def navigation(liste: list, liste_id: list):
     num = input("|  Veuiller rentrer un chiffre ->  ")
-    if num in ["1", "2", "3", "4"]:
+    if num in ["1", "2", "3", "4", "5"]:
         if num == "1":
             ajouter_contact(liste, liste_id)
         elif num == "2":
             supprimer_contact(liste, liste_id)
         elif num == "3":
-            afficher_contacts(liste)
+            rechercher_contact(liste)
         elif num == "4":
+            afficher_contacts(liste)
+        elif num == "5":
             return "quit"
     elif num == "":
         return
@@ -124,8 +127,8 @@ def verif_infos(prenom: str, nom: str, numero: str, email: str):
 
 def ajouter_contact(liste: list, liste_id: list):
     print("\n--- AJOUT CONTACT ---")
-    prenom = input("\nPrénom : ").capitalize()
-    nom = input("Nom : ").capitalize()
+    prenom = input("\nPrénom : ").capitalize().strip()
+    nom = input("Nom : ").capitalize().strip()
     numero = input("Numéro : ")
     email = input("E-mail : ")
     identifiant = generate_id(liste_id)
@@ -161,6 +164,110 @@ def afficher_contacts(liste: list):
     print("\n--- AFFICHAGE DES CONTACTS ---")
     for contact in liste:
         contact.afficher()
+
+
+def rechercher_contact(liste: list):
+    if liste == []:
+        print("\n-- Aucun contact dans la liste de contacts --")
+        return
+    print("""\n
+    --- RECHERCHE ---
+
+-> Types de recherche :
+  1 - Par PRÉNOM ou NOM
+  2 - Par NUMÉRO de téléphone
+  3 - Par ADRESSE E-MAIL
+  4 - Par ID
+  5 - Retour au menu
+
+""")
+
+
+    liste_resultats = navigation_recherche(liste)
+    if liste_resultats == "erreur":
+        return
+    print(f" - Résultat(s) : {len(liste_resultats)} contact(s) trouvé(s)\n")
+    if liste_resultats == "quit":
+        return
+    if len(liste_resultats) == 0:
+        return
+
+
+    contact = choisir_contact(liste_resultats)
+    if contact == "erreur":
+        return
+    contact.afficher()
+
+
+def navigation_recherche(liste: list):
+    print("- Veuillez chosir votre type de recherche")
+    choix = input("   -> ")
+
+    if choix == "1":
+        recherche = input("Nom ou prénom : ").lower()
+        liste_resultats = rechercher_nom_prenom(liste, recherche)
+        return liste_resultats
+
+    elif choix == "2":
+        recherche = input("Numéro : ")
+        liste_resultats = rechercher_numero(liste, recherche)
+        return liste_resultats
+
+    elif choix == "3":
+        recherche = input("Email : ").lower()
+        liste_resultats = rechercher_email(liste, recherche)
+        return liste_resultats
+
+    elif choix == "4":
+        recherche = input("ID : ")
+        liste_resultats = rechercher_id(liste, recherche)
+        return liste_resultats
+    
+    elif choix == "5":
+        return "quit"
+    
+    else:
+        print("-- ERREUR : Choix invalide --")
+        return "erreur"
+
+
+def rechercher_nom_prenom(liste: list, recherche: str):
+    liste_resultats = []
+    for contact in liste:
+        initiale_prenom = (contact.prenom[0]).lower()
+        initiale_nom = (contact.nom[0]).lower()
+        initiale_match = recherche[0] == initiale_prenom or recherche[0] == initiale_nom
+        if len(recherche) == 1 and initiale_match:
+            liste_resultats.append(contact)
+        elif initiale_match and (
+            recherche in (contact.nom).lower() or recherche in (contact.prenom).lower()
+        ):
+            liste_resultats.append(contact)
+    return liste_resultats
+
+
+def rechercher_numero(liste: list, recherche: str):
+    liste_resultats = []
+    for contact in liste:
+        numero = (contact.numero).replace(" ", "")
+        if recherche.replace(" ", "") == numero:
+            liste_resultats.append(contact)
+    return liste_resultats
+
+def rechercher_email(liste: list, recherche: str):
+    liste_resultats = []
+    for contact in liste:
+        initiale_match = recherche[0] == (contact.email[0]).lower()
+        if initiale_match and recherche in (contact.email).lower():
+            liste_resultats.append(contact)
+    return liste_resultats
+
+def rechercher_id(liste: list, recherche: str):
+    liste_resultats = []
+    for contact in liste:
+        if recherche == contact.identifiant:
+            liste_resultats.append(contact)
+    return liste_resultats 
 
 
 def main():
